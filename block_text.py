0561ed5831210497166253f56bb3a28fcb51493f
Jeroen van der Heijden <jvdheijden@insign.it>
2015-10-24 12:24:42 +0200

    .

--- block_text.py
+++ block_text.py
@@ -7,12 +7,18 @@ class BlockText:
 
     def __init__(self, text):
         self._text = self._compile(text)
+        self._need_format = False
 
     def render(self, namespace):
-        return self._text.format_map(namespace.dictionary)
+        print('here', self._need_format)
+        return self._text.format_map(namespace.dictionary) if self._need_format else self._text
 
     def _compile(self, text):
         text = text.replace('{', '{{').replace('}', '}}')
-        text = self.RE_VAR.sub(lambda m: '{' + m.group()[1:] + '}', text)
+        text = self.RE_VAR.sub(self._set_vars, text)
         text = text.replace('@!', '@')
         return text
+
+    def _set_vars(self, m):
+        self._need_format = True
+        return '{' + m.group()[1:] + '}'

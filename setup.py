5d11815cd8de7cc04436de02eab730b511cfdccc
Jeroen van der Heijden <jvdheijden@insign.it>
2016-02-18 21:19:24 +0100

    Package fir PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name='trender',
+      packages=['trender'],
+      version='1.0.2',
+      description='Template Render Engine',
+      author='Jeroen van der Heijden',
+      author_email='jeroen@transceptor.technology',
+      url='https://github.com/transceptor-technology/trender',
+      download_url='https://github.com/transceptor-technology/trender/tarball/1.0.2',
+      keywords=['template', 'engine', 'render'],
+      classifiers=[],
+)

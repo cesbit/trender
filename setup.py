09879a7e450f26812c1c0e18d01f95bfcd6a0e60
Jeroen van der Heijden <jvdheijden@insign.it>
2016-09-21 15:19:44 +0200

    Update version to 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #  Got documentation from: http://peterdowns.com/posts/first-time-with-pypi.html
 #
 #   1. Create tag:
-#       git tag 1.0.4 -m "Adds a tag so that we can put this new version on PyPI."
+#       git tag 1.0.5 -m "Adds a tag so that we can put this new version on PyPI."
 #
 #   2. Push tag:
 #       git push --tags origin master
@@ -20,7 +20,7 @@ from distutils.core import setup
 setup(
     name='trender',
     packages=['trender'],
-    version='1.0.4',
+    version='1.0.5',
     description='Template Render Engine written in pure Python',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
